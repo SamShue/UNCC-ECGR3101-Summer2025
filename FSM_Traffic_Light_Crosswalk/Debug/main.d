9bdfec09eb37f2f0eabbbf96b911257880b8c5d9
Sam Shue <evilrobotfactory@gmail.com>
2025-06-27 07:46:10 -0400

    Traffic Light Interrupts

--- FSM_Traffic_Light_Crosswalk/Debug/main.d
+++ FSM_Traffic_Light_Crosswalk/Debug/main.d
@@ -1,11 +1,11 @@
 # FIXED
 
 main.obj: ../main.c
+main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
 main.obj: C:/ti/ccstheia151/ccs/ccs_base/msp430/include/msp430.h
 main.obj: C:/ti/ccstheia151/ccs/ccs_base/msp430/include/msp430g2553.h
 main.obj: C:/ti/ccstheia151/ccs/ccs_base/msp430/include/in430.h
-main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
-main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
 main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h
 main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
 main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
@@ -20,15 +20,15 @@ main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/
 
 ../main.c:
 
+C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
 C:/ti/ccstheia151/ccs/ccs_base/msp430/include/msp430.h:
 
 C:/ti/ccstheia151/ccs/ccs_base/msp430/include/msp430g2553.h:
 
 C:/ti/ccstheia151/ccs/ccs_base/msp430/include/in430.h:
-
-C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
-
-C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
 
 C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h:
 
--- FSM_Traffic_Light_Crosswalk/main.c
+++ FSM_Traffic_Light_Crosswalk/main.c
@@ -1,3 +1,4 @@
+#include "intrinsics.h"
 #include <msp430.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -23,8 +24,18 @@
 #define BUTTON_OUT_REG      P1OUT
 #define BUTTON_REN_REG      P1REN
 #define BUTTON_IN_REG       P1IN
+#define BUTTON_PIES         P1IES
+#define BUTTON_PIFG         P1IFG
+#define BUTTON_PIE          P1IE
 
 typedef enum {GREEN, YELLOW, RED, CROSSWALK, MAX_STATES} state_t;
+
+typedef struct{
+    bool pressed;
+    uint16_t seconds;
+} inputs_t;
+
+volatile bool globalButtonFlag = false;
 
 void init();
 void delay_s(uint16_t seconds);
@@ -35,16 +46,18 @@ static inline void setRedLed(bool enable);
 static inline void setBlueLed(bool enable);
 static inline bool isButtonPressed();
 
-state_t runGreenState(uint16_t* seconds, bool* pressed);
-state_t runYellowState(uint16_t* seconds, bool* pressed);
-state_t runRedState(uint16_t* seconds, bool* pressed);
-state_t runCrosswalkState(uint16_t* seconds, bool* pressed);
+state_t runGreenState(inputs_t* inputs);
+state_t runYellowState(inputs_t* inputs);
+state_t runRedState(inputs_t* inputs);
+state_t runCrosswalkState(inputs_t* inputs);
 
-state_t (*state_table[MAX_STATES])(uint16_t*, bool*) = {runGreenState, runYellowState, runRedState, runCrosswalkState};
+state_t (*state_table[MAX_STATES])(inputs_t* inputs) = {runGreenState, runYellowState, runRedState, runCrosswalkState};
 
 int main(void) {
-    volatile uint16_t seconds = 0;
-    volatile bool pressed = false;
+    volatile inputs_t inputs;
+    inputs.pressed = false;
+    inputs.seconds = 0;
+
     state_t currentState = GREEN;
     state_t nextState = currentState;
 
@@ -52,40 +65,48 @@ int main(void) {
 
     while(1) {
         if(currentState < MAX_STATES){
-            nextState = state_table[currentState](&seconds, &pressed);
+            nextState = state_table[currentState](&inputs);
         }
 
         currentState = nextState; // Update state
         delay_s(1);
-        seconds++; // Update seconds
-        pressed |= isButtonPressed();
+        inputs.seconds++; // Update seconds
+        inputs.pressed |= globalButtonFlag;
+        globalButtonFlag = false;
     }
 }
 
-state_t runGreenState(uint16_t* seconds, bool* pressed){
+#pragma vector=PORT1_VECTOR
+__interrupt void Port1(void){
+    globalButtonFlag = true;
+    
+    P1IFG &= ~BUTTON_PIN;   // clear interrupt flag
+}
+
+state_t runGreenState(inputs_t* inputs){
     state_t nextState = GREEN;
     setRedLed(false);
     setGreenLed(true);
     setYellowLed(false);
     setBlueLed(false);
 
-    if(*seconds > 5){
-        *seconds = 0;
+    if(inputs->seconds > 5){
+        inputs->seconds = 0;
         nextState = YELLOW;
     }
     return nextState;
 }
 
-state_t runYellowState(uint16_t* seconds, bool* pressed){
+state_t runYellowState(inputs_t* inputs){
     state_t nextState = YELLOW;
     setRedLed(false);
     setGreenLed(false);
     setYellowLed(true);
     setBlueLed(false);
 
-    if(*seconds > 2){
-        *seconds = 0;
-        if(*pressed){
+    if(inputs->seconds > 2){
+        inputs->seconds = 0;
+        if(inputs->pressed){
             nextState = CROSSWALK;
         } else {
             nextState = RED;
@@ -94,31 +115,32 @@ state_t runYellowState(uint16_t* seconds, bool* pressed){
     return nextState;
 }
 
-state_t runRedState(uint16_t* seconds, bool* pressed){
+state_t runRedState(inputs_t* inputs){
     state_t nextState = RED;
     setRedLed(true);
     setGreenLed(false);
     setYellowLed(false);
     setBlueLed(false);
 
-    if(*seconds > 5){
-        *seconds = 0;
+    if(inputs->seconds > 5){
+        inputs->seconds = 0;
         nextState = GREEN;
     }
     return nextState;
 }
 
-state_t runCrosswalkState(uint16_t* seconds, bool* pressed){
+state_t runCrosswalkState(inputs_t* inputs){
     state_t nextState = CROSSWALK;
+
     setRedLed(true);
-    setBlueLed(true);
+    setBlueLed(inputs->seconds % 2);
     setGreenLed(false);
     setYellowLed(false);
 
-    if(*seconds > 3){
-        *seconds = 0;
+    if(inputs->seconds > 8){
+        inputs->seconds = 0;
         nextState = RED;
-        *pressed = false;
+        inputs->pressed = false;
     }
     return nextState;
 }
@@ -132,6 +154,14 @@ void init(){
     YELLOW_LED_DIR_REG |= YELLOW_LED_PIN;
     RED_LED_DIR_REG |= RED_LED_PIN;
     BLUE_LED_DIR_REG |= BLUE_LED_PIN;
+
+    // Button interrupt config
+    BUTTON_DIR_REG &= ~BUTTON_PIN;
+    BUTTON_PIES |= BUTTON_PIN;  // enable high -> low interrupt condition
+    BUTTON_PIFG &= ~BUTTON_PIN;  // clear interrupt flag
+    BUTTON_PIE |= BUTTON_PIN;   // enable interrupt on button pin
+
+    __enable_interrupt();
 }
 
 static inline void setGreenLed(bool enable){
