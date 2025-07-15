d9713f049c7b32bc2576cd9fa7334959d82e1c07
Sam Shue <evilrobotfactory@gmail.com>
2025-07-15 13:51:48 -0400

    PWM Examples

---
+++ Timers_Music_Player/Debug/main.d
@@ -0,0 +1,60 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccstheia151/ccs/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccstheia151/ccs/ccs_base/msp430/include/msp430g2553.h
+main.obj: C:/ti/ccstheia151/ccs/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h
+main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h
+main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h
+main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h
+main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h
+main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h
+main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h
+main.obj: ../notes.h
+main.obj: ../songs.h
+
+../main.c:
+
+C:/ti/ccstheia151/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccstheia151/ccs/ccs_base/msp430/include/msp430g2553.h:
+
+C:/ti/ccstheia151/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h:
+
+C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h:
+
+C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h:
+
+C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h:
+
+C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h:
+
+C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h:
+
+C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h:
+
+C:/ti/ccstheia151/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h:
+
+../notes.h:
+
+../songs.h:
+
---
+++ Timers_PWM_Hardware/main.c
@@ -0,0 +1,20 @@
+#include <msp430.h>
+
+int main(void){
+  WDTCTL = WDTPW + WDTHOLD;       // Stop watchdog timer
+
+  P1DIR |= BIT6;    // Red LED Output
+  P1SEL |= BIT6;    // Red LED PWM
+
+  BCSCTL3 = LFXT1S_2;                      // LFXT1 = VLO
+  IFG1 &= ~OFIFG;                           // Clear OSCFault flag
+
+  TA0CCR0 = 512;    // PWM Period
+  TA0CCR1 = 100;    // PWM duty cycle
+  TA0CCTL1 = OUTMOD_7;  // TA0CCR1 high voltage below count, low voltage above count
+
+  TA0CTL = TASSEL_1 | MC_1 | ID_3;
+
+  while(1){
+  }
+}
